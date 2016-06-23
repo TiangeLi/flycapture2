806ab28b5239cbaf68f9bb07a4fc0129018b588c
tiangeli <tiange.l@hotmail.com>
2016-06-23 13:28:33 -0400

    a

--- src/flycapture2.pyx
+++ src/flycapture2.pyx
@@ -16,6 +16,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 from _FlyCapture2_C cimport *
 include "flycapture2_enums.pxi"
 
