fc1c4cde299ee94c9218382f561790be1ed8eee0
tiangeli <tiange.l@hotmail.com>
2016-06-23 13:42:01 -0400

    a

--- src/flycapture2.pyx
+++ src/flycapture2.pyx
@@ -374,7 +374,8 @@ cdef class Image:
                 self.img.pData, np.NPY_DEFAULT, None)
         r.base = <PyObject *>self
         Py_INCREF(self)
-        return r, self.img.dataSize
+        print self.img.dataSize
+        return r
 
     def get_format(self):
         return self.img.format
